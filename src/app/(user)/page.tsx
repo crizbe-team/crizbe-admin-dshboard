f3aeaf2113c417151a35e21063c5ac7f0d3dd1a0
ahammadsalim1234 <ajmals.oraak@gmail.com>
2026-02-15 18:13:47 +0530

    reload issue fixed

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import { ImageParticles } from '@/components/user/ImageParticles';
 import Hero from '../_components/Hero';
 import QuoteSection from '../_components/QuoteSection';
@@ -7,18 +9,38 @@ import SmoothScroll from '@/components/SmoothScroll';
 import StructuredData from '../_components/StructuredData';
 import FeedbacSection from '../_components/FeedbacSection';
 import Footer from '../_components/Footer';
+import { useEffect, useState } from 'react';
+import UserLoaders from '@/components/ui/UserLoader';
 
 export default function Home() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const handleLoad = () => {
+            setLoading(false);
+        };
+
+        if (document.readyState === "complete") {
+            setLoading(false);
+        } else {
+            window.addEventListener("load", handleLoad);
+            return () => window.removeEventListener("load", handleLoad);
+        }
+    }, []);
     return (
-        <main className="smooth-scroll bg-linear-to-b from-[#FFFAEF] to-[#E3D1A5]">
-            <StructuredData />
-            <SmoothScroll />
-            <Hero />
-            <Flavours />
-            <NextFlavour />
-            <QuoteSection />
-            <FeedbacSection />
-            <Footer />
-        </main>
+        <>
+            {loading && <UserLoaders />}
+            <main className="smooth-scroll bg-linear-to-b from-[#FFFAEF] to-[#E3D1A5]">
+                <StructuredData />
+                <SmoothScroll />
+                <Hero />
+                <Flavours />
+                <NextFlavour />
+                <QuoteSection />
+                <FeedbacSection />
+                <Footer />
+            </main>
+        </>
+
     );
 }
