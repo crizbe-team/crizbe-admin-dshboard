bfa07277dd9b21e67f8ab4c88212ea0a742e4742
swalih-oraak <swalih.oraak@gmail.com>
2026-08-28 11:40:49 +0530

    style: adjust profile layout spacing and sidebar positioning for responsive design

--- src/app/(user)/profile/layout.tsx
+++ src/app/(user)/profile/layout.tsx
@@ -35,15 +35,15 @@ export default function ProfileLayout({
 
     return (
         <div className="min-h-screen bg-[#FCF7EE]">
-            <div className="wrapper py-28">
-                <div className="flex flex-col gap-[30px] lg:flex-row lg:items-start">
-                    <div className="sticky top-[100px] shrink-0 w-full lg:w-[280px]">
+            <div className="wrapper py-20 lg:py-28">
+                <div className="flex flex-col gap-6 lg:gap-[30px] lg:flex-row lg:items-start">
+                    <div className="static lg:sticky lg:top-[100px] shrink-0 w-full lg:w-[280px]">
                         <div className="mb-4 pl-2">
                             <Breadcrumb items={breadcrumbItems} />
                         </div>
                         <ProfileSidebar userName={userName} />
                     </div>
-                    <div className="flex-1 w-full pr-2 pb-10">{children}</div>
+                    <div className="flex-1 w-full lg:pr-2 pb-6 lg:pb-10">{children}</div>
                 </div>
             </div>
         </div>
